f5aade2c8e0a1c80ef2cfc3d1c03132ea2fb224b
tedajax <ted.ajax@gmail.com>
2025-08-21 18:51:02 -0700

    package batch file

---
+++ package.bat
@@ -0,0 +1,26 @@
+@echo off
+
+setlocal EnableDelayedExpansion
+
+set PKGDIR="screenshaver"
+set TARGET="screenshaver"
+set CONFIG="release"
+set PLATFORM="win64"
+set ARCHIVE="%TARGET%.zip"
+set EXECUTABLE="%TARGET%.exe"
+set EXECUTABLE_SRC="bin\%TARGET%\bin\%PLATFORM%\%CONFIG%\%EXECUTABLE%"
+set EXECUTABLE_PKG="%PKGDIR%\%EXECUTABLE%"
+set ASSETSDIR="assets\"
+set ASSETSDIR_PKG="%PKGDIR%\%ASSETSDIR%"
+
+@RD /S /Q %PKGDIR%
+@RD /S /Q %ARCHIVE%
+@mkdir %PKGDIR%
+
+call .\build.bat release
+
+@copy %EXECUTABLE_SRC% %EXECUTABLE_PKG%
+@xcopy /S /Y %ASSETSDIR% %ASSETSDIR_PKG%
+@xcopy /Y *.dll %PKGDIR%\.
+
+7z a %ARCHIVE% %PKGDIR%
